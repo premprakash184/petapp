d6282aa32c3de02c3959e12709f0dabdd716098f
premprakash184 <premprakash184@gmail.com>
2020-08-06 15:50:21 +0530

    display individual pets

--- src/views/Pet.vue
+++ src/views/Pet.vue
@@ -1,6 +1,24 @@
 <template>
   <div>
-    {{ $route.params.species }}
-    {{ $route.params.id }}
+    <h1>{{ animal.name }} ({{ $route.params.species }})</h1>
+    <p>Age: {{ animal.age }} years old</p>
+    <p>Breed: {{ animal.breed }}</p>
   </div>
 </template>
+<script>
+import { mapState } from 'vuex'
+export default {
+  data() {
+    return {
+      animal: {},
+    }
+  },
+  computed: {
+    ...mapState(['cats', 'dogs']),
+  },
+  mounted() {
+    const animals = this[this.$route.params.species][this.$route.params.id]
+    this.animal = animals
+  },
+}
+</script>
